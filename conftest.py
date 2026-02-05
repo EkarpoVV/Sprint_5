93576576c1a2ca17a7388f37844a0fa4a3d36242
EKarpov <username@yandex.ru>
2026-02-05 14:39:55 +0400

    URL to data/urls.py + chenge return driver to yield driver

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
-import test_data as td
+import data.test_data as td
+from data.urls import BASE_URL
 
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
-    driver.get("https://qa-desk.stand.praktikum-services.ru/")
-    return driver
+    driver.get(BASE_URL)
+    yield driver
     driver.quit()
 
